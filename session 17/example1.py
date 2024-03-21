79095aef2d7701f1d22f1fa6bab8971ef65ec5ec
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2024-03-21 09:51:11 +0530

    Add files via upload

---
+++ session 17/example1.py
@@ -0,0 +1,60 @@
+
+
+error
+
+
+// js 
+// let/var/const varname = "string" // typeof - string
+
+varname = 10 // typeof - number
+
+dynamically typed - we can hop from one datatype to another
+strict typed - we cannot hop from one data type to another
+
+
+OOPs - Object Oriented Programming 
+-> Classes - blue-print real world entity - defined a functionality using code 
+            - user defined data type - has a collection of primitive data types
+            - data member and member functions
+            e.g - TV, car, keyboard, mouse
+            - Property (data member)
+                - model number
+                - unique id
+                - model Name
+                - company manufacturer
+                - screen (dimension, resolution, type)
+            - function  
+                - switch on 
+                - switch off
+                - change the channel 
+                    - up and down keys of the remote
+                    - enter the channel number
+                - change the volume
+                - play game 
+                 ......
+-> Objects are the actual implementation of the class
+-> Encapsulation - collection data members and member functions in one unit 
+                of a particular type 
+                - class depicts the real meaning encapsulation 
+                - data hiding - denying the access to change properties of 
+                the class/ object
+                - abstraction - the steps taken in order to perform a particular function
+                that are not needed by the user to know are kept hidden from the User
+-> Inheritance - Parent - Child relationship
+                - Parent is having the general properties and functions
+                - Child is having specification of the parent class' properties and functions 
+                plus its own features
+                - e.g, Shape 
+                    -property
+                        - vertices, edges 
+                    - functionality
+                        - area and perimeter
+                    Child class - Rectangle, Circle, Square
+                    Child class - Cone, cylinder (3d)- combination of - volume
+-> Polymorphism - Poly - different/ many 
+                -morphism - form
+                - function addition(param1, param2){-----}
+                - function addition(param1, param2, param3, param4) {----}
+                - addition(1,3) 
+                - addition(1,3,5,7)
+
